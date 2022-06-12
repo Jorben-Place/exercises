525444c829ee468c8928ecd7ccf8660d7c43e129
Jorben-Place <jorben.place@student.ucll.be>
2022-06-12 19:53:22 +0200

    Update blanket.py

--- old-exams/blanket/blanket.py
+++ old-exams/blanket/blanket.py
@@ -1,10 +1,7 @@
-from datetime import datetime
+import datetime
 import json
-from statistics import mean
-
-
-with open('input.txt') as file:
-    data = [ (datetime.strptime(date, '%d/%m/%Y'), temps) for date, temps in json.load(file).items() ]
-
-for _, temps in sorted(data, key=lambda p: p[0]):
-    print(round(mean(temps)))
+with open('output.txt', 'w') as out:
+    with open('input.txt') as inp:
+        datums = json.loads(inp.read()).items()
+        for key, value in sorted(datums, key=lambda x: datetime.datetime.strptime(x[0], '%d/%m/%Y')):
+            out.write(str(round(sum(value)/len(value)))+'\n')
